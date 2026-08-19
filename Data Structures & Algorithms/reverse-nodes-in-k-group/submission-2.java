ca66a34f959cfcfef093afd49c36c2df3d3e5673
shivammishra9653 <88209080+shivammishra9653@users.noreply.github.com>
2026-08-20 00:23:14 +0530

    Add: reverse-nodes-in-k-group - submission-2

---
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/submission-2.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode getKth(ListNode curr, int k) {
+        while(curr != null && k-- > 0) {
+            curr = curr.next;
+        }
+        return curr;
+    }
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+       ListNode dummy = new ListNode(0, head);
+       ListNode prevGroup = dummy;//this will point to last node of prev group
+
+       while(true) {
+        ListNode kth = getKth(prevGroup, k);
+        if(kth == null) {
+            break;
+        }
+        ListNode prev = null;
+        ListNode curr = prevGroup.next;
+        ListNode groupNext = kth.next;
+        while(curr != groupNext) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        prevGroup.next.next = groupNext; // point next element of previous group to the first element of next group
+        ListNode temp = prevGroup.next;
+        prevGroup.next = prev; //point previousgroup next element to current group first element after reversal
+        prevGroup = temp;// assining reference of current group last element.
+       }
+       return dummy.next;
+    }
+}
