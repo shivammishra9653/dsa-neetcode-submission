fc65a74661a0170e24c7b5ec594f420404f6c616
shivammishra9653 <88209080+shivammishra9653@users.noreply.github.com>
2026-08-21 21:41:57 +0530

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.java
@@ -0,0 +1,47 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<Character> set = new HashSet<>();
+
+        // row
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                if(board[i][j] == '.') continue;
+                if(set.contains(board[i][j])) {
+                    return false;
+                }
+
+                    set.add(board[i][j]);
+            }
+            set.clear();
+        }
+
+        // col
+        for(int i = 0; i < board[0].length; i++) {
+            for(int j = 0; j < board.length; j++) {
+                if(board[j][i] == '.') continue;
+                if(set.contains(board[j][i])) {
+                    return false;
+                }
+                    set.add(board[j][i]);
+            }
+            set.clear();
+        }
+
+        // 3*3 box
+        for(int i = 0; i < board.length; i+=3) {
+            for(int j = 0; j < board[0].length; j+=3) {
+                for(int ii = i; ii < i+3; ii++) {
+                    for(int jj = j; jj < j+3; jj++) {
+                        if(board[ii][jj] == '.') continue;
+                        if(set.contains(board[ii][jj])){
+                            return false;
+                        }
+                            set.add(board[ii][jj]);
+                    }
+                }
+                set.clear();
+            }
+        }
+        return true;
+    }
+}
